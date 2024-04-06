a80f47995c4113a270ef9c46d3eaa3398e917687
Bernardo Oliveira <43864475+helloimbernardo@users.noreply.github.com>
2024-04-06 17:08:05 +0100

    Fixed more obvious issues and set base URL

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/:latitude?/:longitude?/:zoom?" element={<App />} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+const env = loadEnv('', process.cwd());
+
+
+/** defines the base url for the website. this is used for react router as well as others. used as a variable to make configuration simpler */
+const baseURL = '/train-map/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VITE_BASE_URL__: JSON.stringify(baseURL)
+  },
+  base: baseURL,
   plugins: [react()],
-  base: '/train-map/',
 })
